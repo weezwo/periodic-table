8c0bf4d913e9b5e1b34fdc7a5410011c9402dd03
Luisa Scavo <luisascavo@gmail.com>
2021-08-07 19:50:28 -0400

    rewrite valence logic to calc total number of subshells woop woop

--- atomLogic.js
+++ atomLogic.js
@@ -7,27 +7,26 @@ for (i = 0; i <= LARGEST_POSSIBLE_NUMBER_OF_SHELLS; i++) {
    possibleElectronsInOutermostSubshell.push(4 * i + 2) 
 }
 
-  function Atom(protons) {
-    this.atomicNumber = protons;
-    this.name =  ELEMENT_NAMES[this.atomicNumber - 1]
+ 
+function Atom(protons) {
+  this.atomicNumber = protons;
+  this.name =  ELEMENT_NAMES[this.atomicNumber - 1]
+
+  let e = this.atomicNumber
+  let outerCount = 0
+  let totalSubshells = 0
+  do {
+    let innerCount = 0
+    do {
+      e = e - possibleElectronsInOutermostSubshell[innerCount]
+      innerCount++
+      totalSubshells++
+    } while (innerCount <= outerCount && e > 0)
+    outerCount++
    
-    this.calculateValence = () =>  {
+  } while (e > 0)
+  if (e == 0) this.valence = 0
+  else this.valence = -e
+  this.totalSubshells = totalSubshells
+}
 
-      let e = this.atomicNumber
-      let outerCount = 0
-      do {
-        let innerCount = 0
-        do {
-          e = e - possibleElectronsInOutermostSubshell[innerCount]
-          innerCount++
-        } while (innerCount <= outerCount && e > 0)
-        outerCount++
-      } while (e > 0)
-      if (e == 0) return 0
-      else return -e
-    } 
-
-    this.valence = this.calculateValence()
-    this.bonds = 0
-
-  }
